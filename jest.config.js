aeac26f373bd8e8c305abbf7517536a0f8a9c4d3
TysonPWilliams <tyson.williams95@gmail.com>
2025-08-23 20:24:47 +1000

    fix: circular import with the jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 // jest.config.js
 export default {
   testEnvironment: 'node',
-  transform: {}
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapping: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testTimeout: 10000,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 }
