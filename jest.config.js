bcda202f52f7500be7ddee567fb22693459612b8
TysonPWilliams <tyson.williams95@gmail.com>
2025-08-23 20:51:01 +1000

    testing: updated jest to use reporters

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,12 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  testTimeout: 10000
+  testTimeout: 10000,
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './test-results',
+      outputName: 'junit.xml'
+    }]
+  ]
 }
