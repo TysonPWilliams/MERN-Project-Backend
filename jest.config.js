e9a6802da1a8e2d671a41e24d9f664db9c67c1e5
TysonPWilliams <tyson.williams95@gmail.com>
2025-08-23 20:29:20 +1000

    fix: fixed jest config file

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,5 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  moduleNameMapping: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  testTimeout: 10000,
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  testTimeout: 10000
 }
